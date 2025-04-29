890526f82d5488926fe26404b5355830658c5eb3
abdellahcodes <ayoujil.abdellah3@gmail.com>
2025-04-29 19:38:39 +0100

    Initial commit

---
+++ src/main/java/com/spring_rest_crud_api/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.spring_rest_crud_api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ src/test/java/com/spring_rest_crud_api/spring_rest_crud_api/SpringRestCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_rest_crud_api.spring_rest_crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
